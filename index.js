49dea893c6e4d72e3aedd2209af019a070174252
malken21 <Malken_Marumasa@outlook.jp>
2022-09-26 11:51:50 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 const fs = require('fs');
 
-if (!fs.existsSync("./lang")) {
-    fs.mkdirSync("./lang");
-    console.log("./lang/??_??.json");
-    return;
-}
 
 const Config = require("./Config.json");
 
-const main = require(`./lang/${Config.main}`);
-const add = require(`./lang/${Config.add}`);
+//langディレクトリがなかったら 追加
+if (!fs.existsSync("./lang")) {
+    fs.mkdirSync("./lang");
+}
 
-let result = {};
 
-const keys = Object.keys(main);
+const add = require(Config.add);
+const index = require(`${Config.assets}/indexes/${Config.index}`);
+const keys = Object.keys(index.objects);
+
+
 for (let loop = 0; loop < keys.length; loop++) {
+    let key = keys[loop]
 
-    const key = keys[loop];
+    if (key.startsWith("minecraft/lang/")) {//全ての言語が終わるまで繰り返す
+        const file = index.objects[key].hash;
+        key = key.replace("minecraft/lang/", "");
 
-    if (key.startsWith("block.minecraft") || key.startsWith("item.minecraft")) {
-        const text = main[key] + Config.space + add[key];
-        result[keys[loop]] = text;
-        console.log(text)
+        const main = JSON.parse(fs.readFileSync(`${Config.assets}/objects/${file.slice(0, 2)}/${file}`).toString());
+
+        const result = langAdd(main, add);
+
+        fs.writeFile(`./lang/${key}`, JSON.stringify(result, null, "\t"), function (err) {
+            if (!err) {
+                console.log(`finish!! (./lang/${key})`)
+            }
+        })
+
+        console.log(key);
+
     }
 }
 
-console.log(result);
-fs.writeFile('./lang/result.json', JSON.stringify(result, null, "\t"), function (err) {
-    if (!err) {
-        console.log("finish!! (./lang/result.json)")
+
+function langAdd(main, add) {// 後ろに別言語追加 関数
+
+    let result = {};
+
+    const keys = Object.keys(main);
+    for (let loop = 0; loop < keys.length; loop++) {
+
+        const key = keys[loop];
+
+        if (key.startsWith("block.minecraft") || key.startsWith("item.minecraft")) {
+            const text = main[key] + Config.space + add[key];
+            result[keys[loop]] = text;
+        }
     }
-})
+    return result;
+}
